47f35bf90f4d056f7bb21079f0134a0d598d5524
justclaner <justclaner@gmail.com>
2024-08-03 22:29:22 -0400

    change set method to header method

--- backend/auth.js
+++ backend/auth.js
@@ -1,10 +1,10 @@
 const allowCors = (req, res, next) => {
-    res.set('Access-Control-Allow-Credentials', true)
-    res.set('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Origin', '*')
     // another common pattern
     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.set('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.set(
+    res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.header(
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
