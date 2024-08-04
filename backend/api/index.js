0d8cfc47b1952ccab86690737cb307c51306ade1
justclaner <justclaner@gmail.com>
2024-08-03 22:21:15 -0400

    return status 200 for OPTIONS request that cors might send

--- backend/api/index.js
+++ backend/api/index.js
@@ -20,6 +20,10 @@ app.use((req, res, next) => {
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+      }
     next();
   });
 
