92d3b9f733b2775b97c386818143b2298c0c76a4
justclaner <justclaner@gmail.com>
2024-08-03 22:14:23 -0400

    update res.setHeader to res.set

--- backend/auth.js
+++ backend/auth.js
@@ -1,10 +1,10 @@
 const allowCors = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.set('Access-Control-Allow-Credentials', true)
+    res.set('Access-Control-Allow-Origin', '*')
     // another common pattern
     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
+    res.set('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.set(
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
