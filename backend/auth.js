c04bf7637019dd0d3d84aba7157e18798d3e87e8
justclaner <justclaner@gmail.com>
2024-08-04 22:35:04 -0400

    test headers

--- backend/auth.js
+++ backend/auth.js
@@ -3,7 +3,7 @@ const allowCors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://library-client-eight.vercel.app')
     // another common pattern
     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.header('Access-Control-Allow-Methods', 'GET,PATCH,DELETE,POST,PUT,OPTIONS')
     res.header(
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
